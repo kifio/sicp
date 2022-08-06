7d5ba1d8ed3a3da0656bbd75de13a6a2f9383da0
kifio <kg.of.morphine@gmail.com>
2022-08-06 20:18:57 +0300

    Add smallest divisors search and prime numbers search

--- cp1.rkt
+++ cp1.rkt
@@ -94,7 +94,7 @@
 
 (define (even? n) (= (remainder n 2) 0))
 
-; рекурсивное умножение с помощью сложения
+; рекурсивное умножение с помощью сложения (m * n)
 (define (fast-mult-rec m n)
   (cond ((= n 0) 0) ;частный случай
         ((= n 1) m)
@@ -103,7 +103,7 @@
    )
 )
 
-(fast-mult-rec 11 11)
+;(fast-mult-rec 11 11)
 
 
 ; итеративное умножение с помощью сложения (m * n)
@@ -114,4 +114,84 @@
    )
 )
 
-(fast-mult-iter 11 11 0)
+;(fast-mult-iter 11 11 0)
+
+; наименьший делитель
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (divides n divisor)
+  (= (remainder n divisor) 0))
+
+(define (next test-divisor)
+  (if (= test-divisor 2)
+    3
+    (+ test-divisor 2)))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+	((divides n test-divisor) test-divisor)
+	(else (find-divisor n (+ test-divisor 1)))))
+
+; поиск простых чисел в лоб
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+; тест Ферма
+(define (fermat-test n)
+  (define (expmod base exp m)
+  (cond ((= exp 0) 1)
+	((even? exp) (remainder (square (expmod base (/ exp 2) m)) m))
+	(else (remainder (* base (expmod base (- exp 1) m)) m))))
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+	((fermat-test n) (fast-prime? n (- times 1)))
+	(else false)))
+
+;(fast-prime? 29 5)
+(smallest-divisor 199)
+(smallest-divisor 1999)
+(smallest-divisor 19999)
+
+(define (runtime) (current-milliseconds))
+
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+  (when (prime? n)
+    (report-prime (- (runtime) start-time))))
+
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+(timed-prime-test 1999)
+(timed-prime-test 10000000000037)
+(timed-prime-test 10000000000051)
+(timed-prime-test 10000000000099)
+(timed-prime-test 100000000000031)
+(timed-prime-test 100000000000067)
+(timed-prime-test 100000000000097)
+
+(define (search-for-primes n start-time founded-numbers-counter)
+    (cond ((= founded-numbers-counter 0) 
+	    (display "search finished\n"))  
+	  ((prime? n) 
+	    (display n)
+	    (display " founded in: ")
+	    (display (- (runtime) start-time))
+  	    (newline)	    
+ 	    (search-for-primes (+ n 1) (runtime) (- founded-numbers-counter 1)))
+	  (else (search-for-primes (+ n 1) start-time founded-numbers-counter))))
+
+;(search-for-primes 1000000000000  (runtime) 3)
+;(search-for-primes 10000000000000 (runtime) 3)
+;(search-for-primes 100000000000000 (runtime) 3)
+
