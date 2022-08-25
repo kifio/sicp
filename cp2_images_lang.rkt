2aed8dd1e8d63cb6041c471281431efd11a30cd1
kifio <kg.of.morphine@gmail.com>
2022-08-26 00:42:31 +0300

    Drawing on canvas

--- cp2_images_lang.rkt
+++ cp2_images_lang.rkt
@@ -2,8 +2,35 @@
 
 (require "cp2.rkt")
 
-(require racket/draw)
-(require racket/gui)
+(require racket/gui/base)
+
+(define picture-size 500)
+
+(define bitmap
+  (make-object bitmap% (+ picture-size 1) (+ picture-size 1)))
+
+(define bitmap-dc
+  (new bitmap-dc% [bitmap bitmap]))
+
+(define frame
+  (new frame% [label "SICP Picture Language"]))
+
+(define canvas
+  (new canvas%
+     [parent frame]
+     [min-width (+ picture-size 1)]
+     [min-height (+ picture-size 1)]
+     [paint-callback (lambda (canvas dc)
+                       (send dc draw-bitmap bitmap 0 0))]))
+
+(define (draw-line start end)
+  (send bitmap-dc 
+        draw-line
+        (xcor-vect start)
+        (ycor-vect start)
+        (xcor-vect end)
+        (ycor-vect end)))
+
 
 (define (split large-image-layout small-image-layout)
   (define (generic-split painter n) 
@@ -41,6 +68,19 @@
 (define (frame-coord-map frame)
   (lambda (v) (add-vect (origin-frame frame) (add-vect (scale-vect (xcor-vect v) (edge1-frame frame)) (scale-vect (ycor-vect v) (edge2-frame frame))))))
 
+(define window (make-frame (make-vect 0 picture-size)
+                           (make-vect picture-size 0)
+                           (make-vect 0 (- 0 picture-size))))
+
+(define (segments->painter segment-list)
+  (lambda (frame)
+    (for-each
+     (lambda (segment)
+       (draw-line
+        ((frame-coord-map frame) (start-segment segment))
+        ((frame-coord-map frame) (end-segment segment))))
+     segment-list)))
+
 #|(define (screen)
   (let ((origin (make-vect 0 0))
        (edge1 (make-vect 2560 0))
@@ -52,6 +92,43 @@
 (edge2-frame (screen))
 
 ((frame-coord-map (screen)) (make-vect 0.5 0.5))|#
+
+(define (make-segment start end)
+  (list start end))
+
+(define (start-segment segment)
+  (display segment)
+  (newline)
+  (car segment))
+
+(define (end-segment segment)
+  (car (cdr segment)))
+
+(define rect
+  (segments->painter (list
+                      (make-segment (make-vect 0 0) (make-vect 1 0))
+                      (make-segment (make-vect 1 0) (make-vect 1 1))
+                      (make-segment (make-vect 1 1) (make-vect 0 1))
+                      (make-segment (make-vect 0 1) (make-vect 0 0)))))
+
+(define cross
+  (segments->painter (list
+                      (make-segment (make-vect 0 0) (make-vect 1 1))
+                      (make-segment (make-vect 1 0) (make-vect 0 1)))))
+
+(define diamond
+  (segments->painter (list
+                      (make-segment (make-vect 0.5 0) (make-vect 1 0.5))
+                      (make-segment (make-vect 1 0.5) (make-vect 0.5 1))
+                      (make-segment (make-vect 0.5 1) (make-vect 0 0.5))
+                      (make-segment (make-vect 0 0.5) (make-vect 0.5 0)))))
+
+
+#|(send frame show #t)
+(rect window)
+(cross window)
+(diamond window)|#
+;wave рисовать не буду, мне хватило одного перепечатывания координат картины "Кот в сапогах" с миллиметровки в C++ Builder 6. 
 
 #|(define (beside painter1 painter2)
   (let ((split-point (make-vect 0.5 0.0)))
